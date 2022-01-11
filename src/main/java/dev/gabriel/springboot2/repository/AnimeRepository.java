af8bcc40cb82ebc2f3ac4fd3528c08285f39f090
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2022-01-10 21:38:44 -0300

    Aula 16 - RequestParam

--- src/main/java/dev/gabriel/springboot2/repository/AnimeRepository.java
+++ src/main/java/dev/gabriel/springboot2/repository/AnimeRepository.java
@@ -3,6 +3,9 @@ package dev.gabriel.springboot2.repository;
 import dev.gabriel.springboot2.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
 
+    public List<Anime> findByName(String name);
 }
