b90b5fb35b93a03defc08be6cdbbe9fdcfdebf2d
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2022-01-10 23:27:38 -0300

    Aula 20 - Validação de campos - @Valid @NotEmpty @NotNull

--- src/main/java/dev/gabriel/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/dev/gabriel/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,12 @@ package dev.gabriel.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
+    @NotEmpty(message = "The anime name cannot be empty") // Atributo não pode ser vazio
+    // @NotNull faz parte do @NotEmpty
+    // @NotNull(message = "The anime name cannot be null") // Atributo não pode ser nulo
     private String name;
 }
