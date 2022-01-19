3d522e88fd89f58c13ff69a489456fb87871a7c8
Gabriel Sousa <gabrielsousa.ti@gmail.com>
2022-01-18 21:41:08 -0300

    Aula 41 - Spring Security pt 02 - Segurança a nível de métodos com PreAuthori

--- src/main/java/dev/gabriel/springboot2/config/SecurityConfig.java
+++ src/main/java/dev/gabriel/springboot2/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.gabriel.springboot2.config;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 // É um @Configuration e é um @Component
 @EnableWebSecurity
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
